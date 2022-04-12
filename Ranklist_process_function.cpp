5da27b68af567ee9720b2a18e4da77e39325599a
LarryZhang <u3594786@connect.hku.hk>
2022-04-12 17:22:27 +0800

    Change Ranklist_store function into Ranklist_process function

--- Ranklist_process_function.cpp
+++ Ranklist_process_function.cpp
@@ -6,13 +6,42 @@ using namespace std;
 
 // define ranklist store function
 
-void ranklist_store( int rank_list[], int num )
+int ranklist_store( int score )
 {
+	ofstream fin;
+	fin.open("ranklist.txt");
+	int num, ranklist[202];
+	fin >> num;
+	for( int i = 0; i < num; i++ )
+		fin >> ranklist[i];
+	fin.close();
+	if( num + 1 > 200 )
+		ranklist[ num - 1 ] = 0, num--;
+	int ans = -1;
+	for( int i = 0; i < num; i++ )
+	{
+		if( ranklist[i] <= score )
+		{
+			ans = i + 1;
+			for( int j = num; j > i; j-- )
+				ranklist[j] = ranklist[ j - 1 ];
+			num++;
+			ranklist[i] = score;
+			break;
+		}
+	}
+	if( ans == -1 )
+	{
+		ans = num + 1;
+		ranklist[num] = score;
+		num++;
+	}
 	ofstream fout;
 	fout.open("ranklist.txt");
 	fout << num << endl;
 	for( int i = 0; i < num; i++ )
-		fout << rank_list[] << " ";
-	cout << endl;
+		fout << rank_list[i] << " ";
+	fout << endl;
 	fout.close();
+	return ans;
 } 
