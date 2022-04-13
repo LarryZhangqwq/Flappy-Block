cc38178732412778bf2d4c11eb84f3e0e9f11b8f
LarryZhang <u3594786@connect.hku.hk>
2022-04-14 00:16:56 +0800

    Create Refresh scoreboard function files

---
+++ Refresh_scoreboard.cpp
@@ -0,0 +1,10 @@
+#include "Refresh_scoreboard.h"
+#include<iostream>
+using namespace std;
+
+void refresh_scoreborad( char map[], int Now_score, string Name, int Best_score );
+{
+
+
+
+}
---
+++ Refresh_scoreboard.h
@@ -0,0 +1,7 @@
+#ifdef REFRESH_SCOREBOARD_H
+#define REFRESH_SCOREBOARD_H
+
+void refresh_scoreborad( char map[], int Now_score, string Name, int Best_score );
+
+
+#endif 
