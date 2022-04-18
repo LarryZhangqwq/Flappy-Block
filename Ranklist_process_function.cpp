bebbf4bd60be080b7431dab96afdd21a86788d6e
LarryZhang <u3594786@connect.hku.hk>
2022-04-18 10:56:28 +0800

    Add function that only get hightest score

--- Ranklist_process_function.cpp
+++ Ranklist_process_function.cpp
@@ -4,42 +4,55 @@
 #include"Ranklist_process_function.h"
 using namespace std;
 
-int ranklist_store( int score )
+int ranklist_store( int score, int type )
 {
-	ifstream fin;
-	fin.open("ranklist.txt");
-	int num, ranklist[202];
-	fin >> num;
-	for( int i = 0; i < num; i++ )
-		fin >> ranklist[i];
-	fin.close();
-	if( num + 1 > 200 )
-		ranklist[ num - 1 ] = 0, num--;
 	int ans = -1;
-	for( int i = 0; i < num; i++ )
+	if( type == 0 ) //get best score only
 	{
-		if( ranklist[i] <= score )
+		ifstream fin;
+		fin.open("ranklist.txt");
+		int num;
+		fin >> num;
+	 	fin >> ans;		
+		fin.close();
+	}
+		
+	if( type == 1 )
+	{
+		ifstream fin;
+		fin.open("ranklist.txt");
+		int num, ranklist[202];
+		fin >> num;
+		for( int i = 0; i < num; i++ )
+			fin >> ranklist[i];
+		fin.close();
+		if( num + 1 > 200 )
+			ranklist[ num - 1 ] = 0, num--;
+		for( int i = 0; i < num; i++ )
 		{
-			ans = i + 1;
-			for( int j = num; j > i; j-- )
-				ranklist[j] = ranklist[ j - 1 ];
-			num++;
-			ranklist[i] = score;
-			break;
+			if( ranklist[i] <= score )
+			{
+				ans = i + 1;
+				for( int j = num; j > i; j-- )
+					ranklist[j] = ranklist[ j - 1 ];
+				num++;
+				ranklist[i] = score;
+				break;
+			}
 		}
+		if( ans == -1 )
+		{
+			ans = num + 1;
+			ranklist[num] = score;
+			num++;
+		}
+		ofstream fout;
+		fout.open("ranklist.txt");
+		fout << num << endl;
+		for( int i = 0; i < num; i++ )
+			fout << ranklist[i] << " ";
+		fout << endl;
+		fout.close();
 	}
-	if( ans == -1 )
-	{
-		ans = num + 1;
-		ranklist[num] = score;
-		num++;
-	}
-	ofstream fout;
-	fout.open("ranklist.txt");
-	fout << num << endl;
-	for( int i = 0; i < num; i++ )
-		fout << ranklist[i] << " ";
-	fout << endl;
-	fout.close();
 	return ans;
 } 
--- Ranklist_process_function.h
+++ Ranklist_process_function.h
@@ -1,6 +1,6 @@
 # ifndef RANKLIST_READ_FUNCTION_H
 # define RANKLIST_READ_FUNCTION_H
 
-int ranklist_process( int score );
+int ranklist_process( int score, int type );
 
 # endif
