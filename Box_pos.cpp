c9118ab0d58d51c27f0c92eac98fda2119fb2d61
LarryZhangqwq <u3594786@connect.hku.hk>
2022-04-30 12:05:18 +0800

    Update Box_pos

--- Box_pos.cpp
+++ Box_pos.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "conio.h"
 #include <unistd.h>
 using namespace std;
 
@@ -10,13 +11,54 @@ int main()
 {
 	char map[100][100];
 	cout << "col, row, " << endl;
-	int col, row;
-	cin >> col >> row;
+	int col = 60, row = 20;
 	for( int i = 0; i < row; i++ )
 		for( int j = 0; j < col; j++ )
 			map[i][j] = ' ';
-	string c;
-	c = getchar();
-	cout << c << endl;
+	int box_pos = 5;
+	map[box_pos][10] = '#';
+	bool u = 0;
+	while( u != 1 )
+	{
+		while(!getch())
+		{
+			map[box_pos][10] = ' ';
+			box_pos += 1;
+			if( box_pos == row - 1 )
+			{
+				break;
+			       	u = 1;
+			}
+			map[box_pos][10] = '#';
+			for( int i = 0; i < row; i++ )
+			{
+				for( int j = 0; j < col; j++ )
+	                			cout << map[i][j];
+				cout << endl;
+			}
+			usleep(200000);
+			printf("\033[2J\033[1;1H");	
+		}
+	
+		map[box_pos][10] = ' ';
+		box_pos -= 2;
+		if( box_pos < 1 )
+		{
+			break;
+		       	u = 1;
+		}
+		map[box_pos][10] = '#';
+		for( int i = 0; i < row; i++ )
+		{
+			for( int j = 0; j < col; j++ )
+				cout << map[i][j];
+			cout << endl;
+		}
+		usleep(200000);
+                printf("\033[2J\033[1;1H");
+	}
+	cout << 2 << endl;
+
 	return 0;
+
 }
