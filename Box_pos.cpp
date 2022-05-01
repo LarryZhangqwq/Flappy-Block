e40c341ec2d44358c118664e4eeaae1f21b37d9f
LarryZhang <u3594786@connect.hku.hk>
2022-05-01 21:18:37 +0800

    Update box position

--- Box_pos.cpp
+++ Box_pos.cpp
@@ -1,17 +1,38 @@
 #include<iostream>
 #include<algorithm>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include "conio.h"
-#include <unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include"conio.h"
+#include<unistd.h>
+#include <sys/ioctl.h>
+#include <termios.h>
 using namespace std;
+
+char get_keyboard(void) {
+  char input;
+  struct termios new_settings;
+  struct termios stored_settings;
+  tcgetattr(0,&stored_settings);
+  new_settings = stored_settings;
+  new_settings.c_lflag &= (~ICANON);
+  new_settings.c_cc[VTIME] = 0;
+  tcgetattr(0,&stored_settings);
+  new_settings.c_cc[VMIN] = 1;
+  tcsetattr(0,TCSANOW,&new_settings);
+
+  input = getchar();
+
+  tcsetattr(0,TCSANOW,&stored_settings);
+  return input;
+}
 
 int main()
 {
 	char map[100][100];
 	cout << "col, row, " << endl;
-	int col = 60, row = 20;
+	int col, row;
+	cin >> col >> row; 
 	for( int i = 0; i < row; i++ )
 		for( int j = 0; j < col; j++ )
 			map[i][j] = ' ';
@@ -20,7 +41,7 @@ int main()
 	bool u = 0;
 	while( u != 1 )
 	{
-		while(!getch())
+		if(!kbhit())
 		{
 			map[box_pos][10] = ' ';
 			box_pos += 1;
@@ -33,29 +54,34 @@ int main()
 			for( int i = 0; i < row; i++ )
 			{
 				for( int j = 0; j < col; j++ )
-	                			cout << map[i][j];
+	      			cout << map[i][j];
 				cout << endl;
 			}
 			usleep(200000);
 			printf("\033[2J\033[1;1H");	
 		}
-	
-		map[box_pos][10] = ' ';
-		box_pos -= 2;
-		if( box_pos < 1 )
+		else
 		{
-			break;
+			char t; 
+			t = get_keyboard();
+			map[box_pos][10] = ' ';
+			box_pos -= 2;
+			if( box_pos < 1 )
+			{
+				break;
 		       	u = 1;
+			}
+			map[box_pos][10] = '#';
+			for( int i = 0; i < row; i++ )
+			{
+				for( int j = 0; j < col; j++ )
+					cout << map[i][j];
+				cout << endl;
+			}
+			usleep(200000);
+			printf("\033[2J\033[1;1H");
 		}
-		map[box_pos][10] = '#';
-		for( int i = 0; i < row; i++ )
-		{
-			for( int j = 0; j < col; j++ )
-				cout << map[i][j];
-			cout << endl;
-		}
-		usleep(200000);
-                printf("\033[2J\033[1;1H");
+		printf("\033[2J\033[1;1H");
 	}
 	cout << 2 << endl;
 
