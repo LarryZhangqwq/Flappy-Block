b26d7a45f37b1d4dcd7dd11c35d1bd9018d72edc
LarryZhang <u3594786@connect.hku.hk>
2022-05-04 23:03:34 +0800

    Debug + Adjustment parameters

--- Wall_create_function.cpp
+++ Wall_create_function.cpp
@@ -3,7 +3,19 @@
 #include<ctime>
 using namespace std;
 
-int Wall_create_function( char map[200][200], int pre_high, int col, int row ) 
+void print_function( char map[][200], int col, int row )
+{
+	for( int i = 0; i < row; i++ )
+	{
+		for( int j = 0; j < col; j++ )	
+			if( map[i][j] == '#' )
+				printf("\033[0;30;42m \033[0m");
+			else
+				cout << " ";
+		cout << endl;
+	}
+}
+int Wall_create_function( char map[][200], int pre_high, int col, int row ) 
 {
 // --------Create Now_high-------------
 //	int pre_high, row, col; // previous highi
@@ -14,10 +26,10 @@ int Wall_create_function( char map[200][200], int pre_high, int col, int row )
 	int now_type = ( rand() % 2 == 0 )? -1: 1;
 	now_high *= now_type;
 	now_high = now_high % 6 + pre_high;
-	if( now_high <= 3 )
-		now_high += 4;
+	if( now_high <= 5 )
+		now_high += 10;
 	if( now_high + 4 >= row )
-		now_high -= 5;
+		now_high -= 10;
 //	cout << now_high << endl;
 //	cout << now_high << endl;
 // ------------ End ---------------------
@@ -28,7 +40,7 @@ int Wall_create_function( char map[200][200], int pre_high, int col, int row )
 //	for( int i = 0; i < row; i++ )
 //		for( int j = 0; j < col; j++ )	
 //			map[i][j] = ' ';
-	int now_col = 10;
+	int now_col = 70;
 	for( int i = 0; i < row; i++ )
 	{
 		if( i >= now_high - 2 && i <= now_high + 2 )
@@ -37,15 +49,6 @@ int Wall_create_function( char map[200][200], int pre_high, int col, int row )
 			map[i][j] = '#';
 		if( i == now_high - 3 || i == now_high + 3 )
 			map[i][ now_col - width / 2 - 1 ] = '#', map[i][ now_col + width / 2 + 1 ] = '#';
-	}
-	for( int i = 0; i < row; i++ )
-	{
-		for( int j = 0; j < col; j++ )	
-			if( map[i][j] == '#' )
-				printf("\033[0;30;42m \033[0m");
-			else
-				cout << " ";
-		cout << endl;
 	}
 // ---------- End -----------------------
 		
