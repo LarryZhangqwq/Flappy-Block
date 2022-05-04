cd643cacf28f0b5166217f6dccae04b165c9debc
LarryZhang <u3594786@connect.hku.hk>
2022-05-04 23:04:08 +0800

    recombine + debug + Adjustment parameters

--- Move_char.cpp
+++ Move_char.cpp
@@ -6,58 +6,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-//#include "Wall_create_function.cpp"
+#include "Wall_create_function.cpp"
 using namespace std;
-
-int Wall_create_function( char map[][200], int pre_high, int col, int row ) 
-{
-// --------Create Now_high-------------
-//	int pre_high, row, col; // previous highi
-//	cout << "Pre_high, row, col" << endl;
-//	cin >> pre_high >> row >> col;
-	srand(time(0));
-	int now_high = rand();
-	int now_type = ( rand() % 2 == 0 )? -1: 1;
-	now_high *= now_type;
-	now_high = now_high % 6 + pre_high;
-	if( now_high <= 3 )
-		now_high += 4;
-	if( now_high + 4 >= row )
-		now_high -= 5;
-//	cout << now_high << endl;
-//	cout << now_high << endl;
-// ------------ End ---------------------
-
-// ---------Put new wall in map----------
-	int width = 6; // set the width of wall as 12
-//	char map[200][200];
-//	for( int i = 0; i < row; i++ )
-//		for( int j = 0; j < col; j++ )	
-//			map[i][j] = ' ';
-	int now_col = 10;
-	for( int i = 0; i < row; i++ )
-	{
-		if( i >= now_high - 2 && i <= now_high + 2 )
-			continue;
-		for( int j = now_col - width / 2; j <= now_col + width / 2; j++ )
-			map[i][j] = '#';
-		if( i == now_high - 3 || i == now_high + 3 )
-			map[i][ now_col - width / 2 - 1 ] = '#', map[i][ now_col + width / 2 + 1 ] = '#';
-	}
-	for( int i = 0; i < row; i++ )
-	{
-		for( int j = 0; j < col; j++ )	
-			if( map[i][j] == '#' )
-				printf("\033[0;30;42m \033[0m");
-			else
-				cout << " ";
-		cout << endl;
-	}
-// ---------- End -----------------------
-		
-	return now_high;
-}
-
 
 int main()
 {
@@ -70,12 +20,14 @@ int main()
 		for( int j = 0; j <= col; j++ )
 			map[i][j] = ' ';
 // ----- End ------
-	for( int z = 0; z < 10; z++ )
+	for( int z = 0; z < 1000; z++ )
 	{
-		h = Wall_create_function( map, h, col, row );
+		if( z % 15 == 0 )
+			h = Wall_create_function( map, h, col, row );
 		for( int i = 0; i < row; i++ )
 			for( int j = 0; j < col; j++ )
 				map[i][j] = map[i][ j + 1 ];
+		print_function( map, col, row );
 		usleep(200000);
 	}
 	return 0;
