d128a36ff76d5b487777624e9057e455186fc7de
Liu Yitao <ltao@connect.hku.hk>
2022-05-05 18:15:29 +0800

    count marks and judge death

--- Box_pos.cpp
+++ Box_pos.cpp
@@ -1,30 +1,31 @@
-#include<iostream>
-#include<algorithm>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
-#include"conio.h"
-#include<unistd.h>
+#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "conio.h"
+#include <unistd.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 using namespace std;
 
-char get_keyboard(void) {
-  char input;
-  struct termios new_settings;
-  struct termios stored_settings;
-  tcgetattr(0,&stored_settings);
-  new_settings = stored_settings;
-  new_settings.c_lflag &= (~ICANON);
-  new_settings.c_cc[VTIME] = 0;
-  tcgetattr(0,&stored_settings);
-  new_settings.c_cc[VMIN] = 1;
-  tcsetattr(0,TCSANOW,&new_settings);
+char get_keyboard(void)
+{
+	char input;
+	struct termios new_settings;
+	struct termios stored_settings;
+	tcgetattr(0, &stored_settings);
+	new_settings = stored_settings;
+	new_settings.c_lflag &= (~ICANON);
+	new_settings.c_cc[VTIME] = 0;
+	tcgetattr(0, &stored_settings);
+	new_settings.c_cc[VMIN] = 1;
+	tcsetattr(0, TCSANOW, &new_settings);
 
-  input = getchar();
+	input = getchar();
 
-  tcsetattr(0,TCSANOW,&stored_settings);
-  return input;
+	tcsetattr(0, TCSANOW, &stored_settings);
+	return input;
 }
 
 int main()
@@ -33,58 +34,71 @@ int main()
 	cout << "col, row, " << endl;
 	int col, row;
 	cin >> col >> row;
-	for( int i = 0; i < row; i++ )
-		for( int j = 0; j < col; j++ )
+	for (int i = 0; i < row; i++)
+		for (int j = 0; j < col; j++)
 			map[i][j] = ' ';
 	int box_pos = 5;
 	map[box_pos][10] = '#';
 	bool u = 0;
-	while( u != 1 )
+	while (u != 1)
 	{
-		if(!kbhit())
+		if (!kbhit())
 		{
-			map[box_pos][10] = ' ';
-			box_pos += 1;
-			if( box_pos == row - 1 )
+			if (map[box_pos + 1][10] == '#')
 			{
 				break;
-			       	u = 1;
 			}
-			map[box_pos][10] = '#';
-			for( int i = 0; i < row; i++ )
+			else
 			{
-				for( int j = 0; j < col; j++ )
-	      			cout << map[i][j];
-				cout << endl;
+				map[box_pos][10] = ' ';
+				box_pos += 1;
+				if (box_pos == row - 1)
+				{
+					break;
+					u = 1;
+				}
+				map[box_pos][10] = '#';
+				for (int i = 0; i < row; i++)
+				{
+					for (int j = 0; j < col; j++)
+						cout << map[i][j];
+					cout << endl;
+				}
+				usleep(200000);
+				printf("\033[2J\033[1;1H");
 			}
-			usleep(200000);
-			printf("\033[2J\033[1;1H");	
 		}
 		else
 		{
-			char t; 
+			char t;
 			t = get_keyboard();
-			map[box_pos][10] = ' ';
-			box_pos -= 2;
-			if( box_pos < 1 )
+			if (map[box_pos - 2][10] == '#')
 			{
 				break;
-		       	u = 1;
 			}
-			map[box_pos][10] = '#';
-			for( int i = 0; i < row; i++ )
+			else
 			{
-				for( int j = 0; j < col; j++ )
-					cout << map[i][j];
-				cout << endl;
+				map[box_pos][10] = ' ';
+				box_pos -= 2;
+				if (box_pos < 1)
+				{
+					break;
+					u = 1;
+				}
+				map[box_pos][10] = '#';
+				for (int i = 0; i < row; i++)
+				{
+					for (int j = 0; j < col; j++)
+						cout << map[i][j];
+					cout << endl;
+				}
+				usleep(200000);
+				printf("\033[2J\033[1;1H");
 			}
-			usleep(200000);
-			printf("\033[2J\033[1;1H");
 		}
 		printf("\033[2J\033[1;1H");
 	}
 	cout << 2 << endl;
 
 	return 0;
-
 }
---
+++ Count_marks.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Count_marks.h"
+
+void countmarks(char board[][100], int row, int col, int& marks){
+    bool judge1 = 0;
+    bool judge2 = 1;
+    for (int i = 0; i < row; i++){
+        if (board[i][9] == '#'){
+            judge1 = 1;
+        }
+        if (board[i][10] == '#'){
+            judge2 = 0;
+        }
+    }
+    if (judge1 && judge2){
+        marks += 1;
+    }
+}
